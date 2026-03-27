6f563e643b4af4b6bfbe9362573a971bb09313cb
Vanessa Brum-Bastos <vsbrumb@gmail.com>
2026-03-27 16:53:52 +1300

    Update package import comments in mainFunctions.R
    Removed outdated package import comments and added new ones for sf and terra.

--- R/mainFunctions.R
+++ R/mainFunctions.R
@@ -1,31 +1,30 @@
 
 #------------------------------------------------
 # The following commands are needed to ensure that the roxygen2 package, which deals with documenting the package, does not conflict with the Rcpp package.
-
-# Rcpp          - allows C++ integration
-# fftwtools     - fast Fourier transform, used when smoothing posterior draws into final surface
-# ggplot2       - used to produce layered plots
-# ggmap         - needed for the get_map function, although ggmap function itself is broken
-# RColorBrewer  - used to define default colours in geoPlotAllocation
-# rgdal         - required to load shapefiles
-# raster        - required when using masks
-# viridis       - colour palettes
-# ...           - other importFrom declarations recommended by devtools::check
+#------------------------------------------------
+# Rcpp - allows C++ integration
+# fftwtools - fast Fourier transform
+# ggplot2 - layered plots
+# RColorBrewer - default colours
+# sf - vector spatial data I/O and CRS handling
+# terra - raster/vector operations and distance masking
+# viridis - colour palettes
+# ...
 
 #' @useDynLib RgeoProfile
 #' @importFrom Rcpp evalCpp
 #' @import fftwtools
 #' @import ggplot2
-#' @import ggmap
 #' @import RColorBrewer
-#' @importFrom sf st_read
-#' @importFrom raster raster extent extent<- rasterize projectRaster distance
 #' @import viridis
+#' @importFrom sf st_as_sf st_bbox st_crs st_geometry st_read st_transform
+#' @importFrom terra crs distance ext project rast rasterize vect
 #' @importFrom grDevices colorRampPalette
 #' @import graphics
 #' @import stats
 #' @import utils
 NULL
+#------------------------------------------------
 
 #------------------------------------------------
 #' Create Rgeoprofile data object
